76814b566c17b5b718ab25a520292e39f2d81e20
Yigit Erzen <yigterzen@gmail.com>
2025-09-15 22:28:41 +0300

    Enhance NewExpense widget with amount input and date picker functionality

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -12,12 +12,24 @@ class NewExpense extends StatefulWidget {
 class _NewExpenseState extends State<NewExpense> {
   
   final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
   @override
   void dispose() {
     _titleController.dispose();
+    _amountController.dispose();
     super.dispose();
   }
 
+  void _PresentDatePicker(){
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+    showDatePicker(
+      context: context, 
+      initialDate: now, 
+      firstDate: firstDate, 
+      lastDate: now,
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -29,10 +41,46 @@ class _NewExpenseState extends State<NewExpense> {
             maxLength: 50,
             decoration: InputDecoration(labelText: 'Title'),
           ),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _amountController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    prefixText: '\$ ',
+                    labelText: 'Amount',
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text('Date'),
+                    IconButton(
+                      onPressed: () {
+                        _PresentDatePicker();
+                      },
+                      icon: Icon(Icons.calendar_month),
+                    )
+                  ],
+                )
+              )
+            ],
+          ),
           Row(children: [
+            TextButton  (
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Cancel'),
+            ),
             ElevatedButton(
               onPressed: () {
                 print(_titleController.text);
+                print(_amountController.text);
               },
               child: const Text('Save Expense'),
             )
