a44e400784fe5fa99c3423a1294a73d7e64b5609
Yigit Erzen <yigterzen@gmail.com>
2025-09-22 19:02:33 +0300

    Implement expense removal with undo functionality and update main content display in Expenses widget

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -43,14 +43,42 @@ class _ExpensesState extends State<Expenses> {
   }
 
   void _removeExpense(Expense expense) {
+    final expenseIndex = _registeredExpenses.indexOf(expense);
     setState(() {
-      _registeredExpenses.add(expense);
+      _registeredExpenses.remove(expense);
     });
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+      content: const Text('Expense deleted.'),
+      duration: const Duration(seconds: 3),
+      action: SnackBarAction(
+        label: 'Undo',
+        onPressed: () {
+          setState(() {
+            _registeredExpenses.insert(expenseIndex, expense);
+          });
+        },
+       ),
+      ),
+    );
   }
 
 
   @override
   Widget build(BuildContext context) {
+  
+  Widget mainContent = const Center(
+    child: Text('No expenses found. Start adding some!'),
+  );
+
+   if (_registeredExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+        expenses: _registeredExpenses,
+        onRemoveExpense: _removeExpense,
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter Expense Tracker'),
@@ -66,7 +94,7 @@ class _ExpensesState extends State<Expenses> {
       body: Column(
       children:[
         Text('The Chart'),
-        Expanded(child: ExpensesList(expenses: _registeredExpenses, onRemoveExpense: _removeExpense,)),
+        Expanded(child: mainContent),
       ]
     ),
     );
